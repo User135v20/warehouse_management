1b2a1943ae4cc3305fcafad27f18d931eb7d77f4
User135v20 <gra432322@gmail.com>
2026-03-09 15:06:32 +0300

    warehouse management

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ from infrastructure.unit_of_work import SqlAlchemyUnitOfWork
 from infrastructure.database import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
-SessionFactory=sessionmaker(bind=engine)
+SessionFactory = sessionmaker(bind=engine)
 Base.metadata.create_all(engine)
+
 
 def main():
     session = SessionFactory()
@@ -25,24 +26,16 @@ def main():
         uow.commit()
         list_product = warehouse_service.list_product()
         print(list_product)
-
-        warehouse_service.get_product(1)
-
-        print(f"create product: {new_product}")
-        # todo add some actions
-
-
-        new_product2 = warehouse_service.create_product(name="test123", quantity=1, price=101)
+        print(warehouse_service.get_product(1))
+        warehouse_service.create_product(name="test123", quantity=1, price=101)
         uow.commit()
         print(warehouse_service.list_product())
-        #new_product2 = warehouse_service.create_product(name="test123", quantity=1, price=100)
         uow.commit()
         list_product = warehouse_service.list_product()
         print(list_product)
-        warehouse_service.create_order(products=list([warehouse_service.get_product(1), warehouse_service.get_product(2)]))
-        #warehouse_service.create_order(products=list([warehouse_service.get_product(1), Product(id=10, name="test",quantity=1,price=100)]))
+        warehouse_service.create_order(
+            products=list([warehouse_service.get_product(1), warehouse_service.get_product(2)]))
         print(warehouse_service.list_orders())
-        print(warehouse_service.get_order(111))
 
 
 if __name__ == "__main__":
